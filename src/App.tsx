e60e080d014a0435fab0873ef50da43fcc54d9e4
José Francisco Cruz Arroyo <jose.cruz.makingdevs@kueski.com>
2022-08-08 00:29:18 -0500

    creates master creates cart and details sections components

--- src/App.tsx
+++ src/App.tsx
@@ -8,31 +8,19 @@ import {
   Code,
   Grid,
   theme,
+  Container,
+  Flex,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import Details from "./sections/details"
+import Cart from "./sections/cart"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
+    <Container maxW="container.xl" p={0}>
+      <Flex h="100vh" py={20}>
+        <Details />
+        <Cart />
+      </Flex>
+    </Container>
   </ChakraProvider>
 )
