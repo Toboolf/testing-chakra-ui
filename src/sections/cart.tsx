97f82278bfa4c3f3ad2f1686b4837ca66ff89545
José Francisco Cruz Arroyo <jose.cruz.makingdevs@kueski.com>
2022-08-11 00:00:58 -0500

    adds master adds cart detail full tags and toggle dark mode

--- src/sections/cart.tsx
+++ src/sections/cart.tsx
@@ -1,15 +1,21 @@
 import {
   AspectRatio,
   Button,
+  Divider,
   Heading,
   HStack,
   Image,
   Stack,
   Text,
+  useColorMode,
+  useColorModeValue,
   VStack,
 } from "@chakra-ui/react";
 
 const Cart = () => {
+  const { toggleColorMode } = useColorMode();
+  const bgColor = useColorModeValue("gray.50", "whiteAlpha.50");
+  const secondaryTextColor = useColorModeValue("gray.600", "gray.400");
   return (
     <VStack
       w="full"
@@ -17,13 +23,13 @@ const Cart = () => {
       p={10}
       spacing={6}
       alignItems="flex-start"
-      bg="gray.50"
+      bg={bgColor}
     >
       <VStack alignItems="flex-start" spacing={3}>
         <Heading size="2xl">Your cart</Heading>
         <Text>
           If the price is too hard on your eyes,{" "}
-          <Button variant="link" colorScheme="black">
+          <Button variant="link" colorScheme="black" onClick={toggleColorMode}>
             try changing the theme.
           </Button>
         </Text>
@@ -34,20 +40,50 @@ const Cart = () => {
             src="https://m.media-amazon.com/images/I/81-erM+IbyL._AC_SL1500_.jpg"
             alt="Penny Board"
           />
-          <Stack
-            spacing={0}
-            w="full"
-            direction="row"
-            justifyContent="space-between"
-            alignItems="center"
-          >
-            <VStack w="full" spacing={0} alignItems="center">
-              <Heading size="md">Penny board</Heading>
-              <Text color="gray.600">PNYCOMP27541</Text>
-            </VStack>
-          </Stack>
         </AspectRatio>
+        <Stack
+          spacing={0}
+          w="full"
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <VStack w="full" spacing={0} alignItems="center">
+            <Heading size="md">Penny board</Heading>
+            <Text color={secondaryTextColor}>PNYCOMP27541</Text>
+          </VStack>
+          <Heading size="sm" textAlign="end">
+            $119.00
+          </Heading>
+        </Stack>
       </HStack>
+      <VStack spacing={4} alignItems="stretch" w="full">
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Subtotal</Text>
+          <Heading size="sm" textAlign="end">
+            $119.00
+          </Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Shipping</Text>
+          <Heading size="sm" textAlign="end">
+            $19.99
+          </Heading>
+        </HStack>
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Taxes(estimated)</Text>
+          <Heading size="sm" textAlign="end">
+            $23.80
+          </Heading>
+        </HStack>
+        <Divider />
+        <HStack justifyContent="space-between">
+          <Text color={secondaryTextColor}>Total</Text>
+          <Heading size="lg" textAlign="end">
+            $162.79
+          </Heading>
+        </HStack>
+      </VStack>
     </VStack>
   );
 };
