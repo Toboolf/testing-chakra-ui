c50455c2bd6e66412542e4fb77eb116aea1300ea
José Francisco Cruz Arroyo <jose.cruz.makingdevs@kueski.com>
2022-08-11 00:13:30 -0500

    [master] makes page responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  theme,
-  Container,
-  Flex,
-} from "@chakra-ui/react"
-import Details from "./sections/details"
-import Cart from "./sections/cart"
+import * as React from "react";
+import { ChakraProvider, theme, Container, Flex } from "@chakra-ui/react";
+import Details from "./sections/details";
+import Cart from "./sections/cart";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Container maxW="container.xl" p={0}>
-      <Flex h="100vh" py={20}>
+      <Flex
+        h={{ base: "auto", md: "100vh" }}
+        py={[0, 10, 20]}
+        direction={{ base: "column-reverse", md: "row" }}
+      >
         <Details />
         <Cart />
       </Flex>
     </Container>
   </ChakraProvider>
-)
+);
